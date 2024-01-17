90734daa6b46059bd56e6ce88506e1544e82bf60
Seva <seva.leonov@hotmail.com>
2024-01-17 14:42:12 +0700

    Move components to a separate folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Router from "./Router";
+import Router from "./components/Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
