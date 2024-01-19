ee8824493c1323bfb4606dbe92aaa738eff51101
Seva <seva.leonov@hotmail.com>
2024-01-19 11:49:58 +0700

    Add ScrollToTop feature

--- src/components/CollectionProducts.jsx
+++ src/components/CollectionProducts.jsx
@@ -3,6 +3,7 @@ import Navigation from "./Navigation.jsx";
 import Footer from "./Footer.jsx";
 import { useCollections } from "./CollectionsContext.jsx";
 import Content from "./Content.jsx";
+import ScrollToTop from "./ScrollToTop.jsx";
 
 function CollectionProducts() {
   const collections = useCollections();
@@ -20,6 +21,7 @@ function CollectionProducts() {
 
   return (
     <>
+      <ScrollToTop />
       <Navigation />
       {selectedCollection ? (
         <Content type="products" selectedCollection={selectedCollection[0]} />
