b904d406965d7806490d3e2cc883e31cd4e70b42
Seva <seva.leonov@hotmail.com>
2024-01-25 16:14:33 +0700

    Add close button to cart menu

--- src/components/sections/Cart.jsx
+++ src/components/sections/Cart.jsx
@@ -10,12 +10,21 @@ function Cart({ onClick }) {
         className="fixed z-10 h-full w-full bg-black opacity-40 blur-lg"
         onClick={onClick}
       ></div>
-      <div className=" fixed right-0 top-0 z-20 flex h-full w-1/3 min-w-96 flex-col overflow-auto bg-white">
+      <div className="fixed right-0 top-0 z-20 flex h-full w-1/3 min-w-96 flex-col overflow-auto bg-white">
         {cartContext.cart.length > 0 ? (
           <>
-            <p className="pb-3 pl-5 pr-5 pt-3 text-2xl">
-              Cart Items: {cartContext.quantity}
-            </p>
+            <div className="relative">
+              <p className="pb-3 pl-5 pr-5 pt-3 text-2xl">
+                Cart Items: {cartContext.quantity}
+              </p>
+              <div
+                className="absolute right-[10px] top-[10px] hover:cursor-pointer"
+                onClick={onClick}
+              >
+                <span className="material-symbols-outlined">close</span>
+              </div>
+            </div>
+
             {cartContext.cart.map((item) => (
               <Card
                 key={item.title}
